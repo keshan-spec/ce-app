f6e6547bacdd477154dce400f28502f00272cd8b
Keshanth Jude <keshanth64@gmail.com>
2024-05-17 16:07:03 +0100

    Updated post pfp

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -70,6 +70,8 @@ const PostCard = ({ post, muted, setMuted, openComments }: {
     openComments: (postId: number) => void;
 }) => {
     const { isLoggedIn, user } = useUser();
+    console.log("PostCard -> user", user);
+    console.log("PostCard -> post", post);
 
     const videoRef = useRef<HTMLVideoElement>(null);
     const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [AutoHeight({ delay: 5000, stopOnInteraction: false })]);
@@ -276,16 +278,18 @@ const PostCard = ({ post, muted, setMuted, openComments }: {
         <div className="relative shadow-md overflow-hidden bg-theme-dark mb-6 text-white" id={`PostMain-${post.id}`}>
             <div className="flex items-center justify-between px-3 py-3">
                 <div className="flex flex-col items-start justify-start">
-                    <div className="avatar">
-                        <div className="w-8 h-8 bg-gray-300 rounded-full border-1 border-theme-primary mt-1">
-                            <img
-                                src={user?.profile_image}
-                                alt="User Avatar"
-                                className="w-full h-full object-cover rounded-full"
-                            />
+                    <div className="flex items-center gap-2 justify-center">
+                        <div className="avatar">
+                            <div className="w-8 h-8 bg-gray-300 rounded-full border-1 border-theme-primary">
+                                <img
+                                    src={user?.profile_image}
+                                    alt="User Avatar"
+                                    className="w-full h-full object-cover rounded-full"
+                                />
+                            </div>
                         </div>
+                        <div className="font-medium text-white text-sm">{post.username}</div>
                     </div>
-                    <div className="font-medium text-white text-sm">{post.username}</div>
                     <div className="flex items-center gap-2 text-xs text-white/60">
                         {post.location && (
                             <div className="flex items-center gap-1">
