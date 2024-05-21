0490492a112dea1ab7247da036591145fbeedeeb
Keshanth Jude <keshanth64@gmail.com>
2024-05-21 15:33:01 +0100

    test: No SEO

--- src/app/(protected)/posts/[id]/page.tsx
+++ src/app/(protected)/posts/[id]/page.tsx
@@ -4,49 +4,49 @@ import { fetchPost } from '@/actions/post-actions';
 import PostClient from './PostClient';
 
 // SEO
-type Props = {
-    params: { id: string; };
-    searchParams: { [key: string]: string | string[] | undefined; };
-};
+// type Props = {
+//     params: { id: string; };
+//     searchParams: { [key: string]: string | string[] | undefined; };
+// };
 
-export async function generateMetadata(
-    { params, searchParams }: Props,
-    parent: ResolvingMetadata
-): Promise<Metadata> {
-    // read route params
-    const id = params.id;
-    const post = await fetchPost(id);
-    const previousImages = (await parent).openGraph?.images || [];
+// export async function generateMetadata(
+//     { params, searchParams }: Props,
+//     parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//     // read route params
+//     const id = params.id;
+//     const post = await fetchPost(id);
+//     const previousImages = (await parent).openGraph?.images || [];
 
-    let title: string;
-    let image: string;
+//     let title: string;
+//     let image: string;
 
-    if (!post) {
-        title = 'Post not found';
-        image = '';
-    } else {
-        if (post.caption) {
-            title = post.caption;
-        } else {
-            title = 'Post';
-        }
+//     if (!post) {
+//         title = 'Post not found';
+//         image = '';
+//     } else {
+//         if (post.caption) {
+//             title = post.caption;
+//         } else {
+//             title = 'Post';
+//         }
 
-        image = post.media[0].media_url;
-    }
+//         image = post.media[0].media_url;
+//     }
 
-    return {
-        title,
-        description: `${post?.likes_count} likes, ${post?.comments_count} comments - ${post?.username}`,
-        openGraph: {
-            images: [
-                ...previousImages,
-                { url: image },
-            ],
-            type: 'article',
-            siteName: 'Drive Life',
-        },
-    };
-}
+//     return {
+//         title,
+//         description: `${post?.likes_count} likes, ${post?.comments_count} comments - ${post?.username}`,
+//         openGraph: {
+//             images: [
+//                 ...previousImages,
+//                 { url: image },
+//             ],
+//             type: 'article',
+//             siteName: 'Drive Life',
+//         },
+//     };
+// }
 
 const Page = ({ params }: { params: { id: string; }; }) => {
     return <PostClient postId={params.id} />;
