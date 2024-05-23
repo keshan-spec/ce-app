1a5133ecbe2ef6176bcadc0dbd3a1ff4d13c371e
Keshanth Jude <keshanth64@gmail.com>
2024-05-23 16:28:56 +0100

    reverted: view profile as client

--- src/app/(protected)/profile/[id]/page.tsx
+++ src/app/(protected)/profile/[id]/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import { getUserDetails } from "@/actions/auth-actions";
 import { ProfileLayout } from "@/components/Profile/ProfileLayout";
 import { Metadata, ResolvingMetadata } from "next";
@@ -7,61 +6,49 @@ type Props = {
     params: { id: string; };
 };
 
-// export async function generateMetadata(
-//     { params }: Props,
-//     parent: ResolvingMetadata
-// ): Promise<Metadata> {
-//     // read route params
-//     const id = params.id;
-//     const post = await getUserDetails(id);
-//     const previousImages = (await parent).openGraph?.images || [];
+export async function generateMetadata(
+    { params }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    // read route params
+    const id = params.id;
+    const post = await getUserDetails(id);
+    const previousImages = (await parent).openGraph?.images || [];
 
-//     let title: string = 'User';
-//     let image: string;
-//     let description = 'User Profile';
+    let title: string = 'User';
+    let image: string;
+    let description = 'User Profile';
 
-//     if (!post) {
-//         title = 'User not found';
-//         image = '';
-//     } else {
-//         if (post.success) {
-//             title = post.user?.username || 'User';
-//         }
+    if (!post) {
+        title = 'User not found';
+        image = '';
+    } else {
+        if (post.success) {
+            title = post.user?.username || 'User';
+        }
 
-//         image = post.user?.profile_image || '';
-//         description = `${post.user?.first_name} ${post.user?.last_name} - ${post.user?.posts_count} posts | ${post.user?.followers.length} followers | ${post.user?.following.length} following`;
-//     }
+        image = post.user?.profile_image || '';
+        description = `${post.user?.first_name} ${post.user?.last_name} - ${post.user?.posts_count} posts | ${post.user?.followers.length} followers | ${post.user?.following.length} following`;
+    }
 
-//     return {
-//         title: `@${title} Profile | Drive Life`,
-//         description,
-//         openGraph: {
-//             images: [
-//                 ...previousImages,
-//                 { url: image },
-//             ],
-//             firstName: post?.user?.first_name,
-//             lastName: post?.user?.last_name,
-//             username: post?.user?.username,
-//             type: 'profile',
-//             siteName: 'Drive Life',
-//         },
-//     };
-// }
-
+    return {
+        title: `@${title} Profile | Drive Life`,
+        description,
+        openGraph: {
+            images: [
+                ...previousImages,
+                { url: image },
+            ],
+            firstName: post?.user?.first_name,
+            lastName: post?.user?.last_name,
+            username: post?.user?.username,
+            type: 'profile',
+            siteName: 'Drive Life',
+        },
+    };
+}
 
 const Page = async ({ params }: { params: { id: string; }; }) => {
-    // const session = await auth();
-    // const user = await getUserDetails(params.id);
-
-    // if (!user || !user.success) {
-    //     return <UserNotFound />;
-    // }
-
-    // return (
-    //     <DLayout sessionUser={session?.user!} user={user.user!} profileId={params.id} />
-    // );
-
     return (
         <div className="relative min-h-[150dvh]">
             <ProfileLayout currentUser={false} profileId={params.id} />
