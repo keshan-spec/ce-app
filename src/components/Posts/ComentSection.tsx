46a4d461822afec779f1680a689914228ce7364a
Keshanth Jude <keshanth64@gmail.com>
2024-05-23 16:47:59 +0100

    Updated tanstack query v5

--- src/components/Posts/ComentSection.tsx
+++ src/components/Posts/ComentSection.tsx
@@ -24,7 +24,7 @@ export const ComentsSection: React.FC<ComentsSectionProps> = ({
         retry: 0,
         refetchOnWindowFocus: false,
         refetchOnMount: false,
-        keepPreviousData: true,
+        // keepPreviousData: true,
     });
 
     const onNewComment = () => {
