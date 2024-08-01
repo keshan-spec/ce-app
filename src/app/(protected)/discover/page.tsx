ac55a179aee1c052d3a760b54ebc866e9fa85aa0
Keshanth Jude <keshanth64@gmail.com>
2024-08-01 16:23:48 +0100

    Removed prefetch

--- src/app/(protected)/discover/page.tsx
+++ src/app/(protected)/discover/page.tsx
@@ -1,28 +1,7 @@
-import { fetchTrendingEvents, fetchTrendingVenues } from "@/actions/home-actions";
 import DiscoverAndSearchPage from "@/components/Home/Home";
-import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
-
-const queryPrefetchPromises = () => {
-    return [
-        queryClient.prefetchQuery({
-            queryKey: ["trending-events"],
-            queryFn: () => fetchTrendingEvents(1),
-        }),
-        queryClient.prefetchQuery({
-            queryKey: ["trending-venues"],
-            queryFn: () => fetchTrendingVenues(1),
-        }),
-    ];
-};
 
 export default async function Page() {
-    await Promise.all(queryPrefetchPromises());
-
     return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <DiscoverAndSearchPage />
-        </HydrationBoundary>
+        <DiscoverAndSearchPage />
     );
 }
